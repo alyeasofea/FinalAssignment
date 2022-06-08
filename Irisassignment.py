50ef951cac6d17a46be94ce05cb41412200d2b84
alyeasofea <106502776+alyeasofea@users.noreply.github.com>
2022-06-09 01:06:26 +0800

    Update Irisassignment.py

--- Irisassignment.py
+++ Irisassignment.py
@@ -27,9 +27,9 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
+iris = pd.read_csv('https://raw.githubusercontent.com/alyeasofea/FinalAssignment/main/Irisassignment.py')
+X = data.drop('species', axis = 1)
+Y = y = data['species']
 
 clf = RandomForestClassifier()
 clf.fit(X, Y)
@@ -38,11 +38,14 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
+st.write(pd.DataFrame(
+ { 0 : 'iris-setosa', 
+1 : 'iris-versicolor', 
+2: 'iris-virginica'})
 
 st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-#st.write(prediction)
+#st.write(iris.target_names[prediction])
+st.write(prediction)
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
